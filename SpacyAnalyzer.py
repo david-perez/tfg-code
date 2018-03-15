8f181c7a4ee72fb6908223df6a6e48576d67a333
david-perez <hallaplay835@gmail.com>
2018-03-15 19:11:30 +0100

    SpacyAnalyzer: skeleton

---
+++ SpacyAnalyzer.py
@@ -0,0 +1,33 @@
+import en_core_web_sm
+import spacy
+
+
+class SpacyAnalyzer:
+    def __init__(self):
+        self.nlp = en_core_web_sm.load(disable=['tagger', 'parser', 'ner'])
+
+        def remove_punctuation(doc):
+            return [token for token in doc if not token.is_punct]
+
+        def remove_numerical_tokens(doc):
+            return [token for token in doc if not any(char.isdigit() for char in token.lower_)]
+
+        def remove_stop_words(doc):
+            # Stop words in spaCy are lowercase.
+            return [token for token in doc if not token.lower_ in spacy.lang.en.stop_words.STOP_WORDS]
+
+        def remove_whitespace(doc):
+            return [token for token in doc if not token.is_space]
+
+        def remove_one_word_tokens(doc):
+            return [token for token in doc if len(token) > 1]
+
+        self.nlp.add_pipe(remove_stop_words, name='remove_stop_words', first=True)
+        self.nlp.add_pipe(remove_one_word_tokens, name='remove_one_word_tokens', first=True)
+        self.nlp.add_pipe(remove_numerical_tokens, name='remove_numerical_tokens', first=True)
+        self.nlp.add_pipe(remove_whitespace, name='remove_whitespace', first=True)
+        self.nlp.add_pipe(remove_punctuation, name='remove_punctuation', first=True)
+
+    def analyze(self, text):
+        doc = self.nlp(text)
+        return [token.lower_ for token in doc]
