f008de77931fd0ea4aff60f91b05a7ffd69fb51e
david-perez <hallaplay835@gmail.com>
2018-03-18 23:35:09 +0100

    Update DatabaseManager, icd9_codes helper

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -1,5 +1,6 @@
 from configparser import ConfigParser
 
+import pickle
 import psycopg2
 
 
@@ -42,6 +43,75 @@ class DatabaseManager:
 
         return conn
 
+    def get_corpus_training_set(self, toy_set=None, top10_labels=True, top100_labels=False):
+        assert(top10_labels ^ top100_labels)
+
+        cur = self.__conn.cursor()
+
+        if top10_labels:
+            if toy_set is None:
+                cur.execute('SELECT * FROM training_set_top10_labels ORDER BY subject_id ASC, r ASC')
+            else:
+                cur.execute('SELECT * FROM training_set_top10_labels ORDER BY subject_id ASC, r ASC LIMIT %s', (toy_set,))
+        elif top100_labels:
+            pass
+
+        subject_ids = []
+        notes = []
+        for row in cur:
+            notes.append(row[5])
+            subject_ids.append(row[1])
+
+        return subject_ids, notes
+
+    def insert_bag_of_words_vectors_training_set(self, bag_of_words_vectors, toy_set=None, top10_labels=True, top100_labels=False):
+        assert(top10_labels ^ top100_labels)
+
+        cur = self.__conn.cursor()
+
+        if top10_labels:
+            for subject_id, how_many_notes, bag_of_words_col_ind, bag_of_words_data in bag_of_words_vectors:
+                cur.execute('INSERT INTO bag_of_words_training_set_top10_labels VALUES (%s, %s, %s, %s)',
+                            (subject_id, how_many_notes,
+                             psycopg2.Binary(pickle.dumps(bag_of_words_col_ind)),
+                             psycopg2.Binary(pickle.dumps(bag_of_words_data))))
+        elif top100_labels:
+            pass
+
+        self.__conn.commit()
+
+    def get_icd9_codes(self, top10_labels=True, top100_labels=False, subject_id=None):
+        assert((top10_labels ^ top100_labels) or (subject_id is not None))
+
+        cur = self.__conn.cursor()
+
+        if subject_id is None:
+            if top10_labels:
+                cur.execute('SELECT icd9_code FROM top10_labels ORDER BY icd9_code ASC')
+            elif top100_labels:
+                pass
+        else:
+            cur.execute("""SELECT icd9_code FROM training_set_top10_labels_patients_and_diagnoses WHERE subject_id = %s
+                        ORDER BY icd9_code ASC""", (subject_id,))
+
+        return [row[0] for row in cur.fetchall()]
+
     def __close_connection(self):
         if self.__conn is not None:
             self.__conn.close()
+
+    def get_bag_of_words_vectors_training_set(self, toy_set=None, top10_labels=True, top100_labels=False):
+        assert(top10_labels ^ top100_labels)
+
+        cur = self.__conn.cursor()
+
+        if top10_labels:
+            if toy_set is None:
+                cur.execute('SELECT * FROM bag_of_words_training_set_top10_labels ORDER BY subject_id ASC')
+            else:
+                cur.execute('SELECT * FROM bag_of_words_training_set_top10_labels ORDER BY subject_id ASC LIMIT %s',
+                            (toy_set,))
+        elif top100_labels:
+            pass
+
+        return cur
