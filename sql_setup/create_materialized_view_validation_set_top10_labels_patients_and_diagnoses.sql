f1927f26632cb139ca873456bfe2d699356b6ab9
David Pérez <davidp00@ucm.es>
2018-04-23 00:53:58 +0200

    Bugfix materialized view validation set creation script

--- sql_setup/create_materialized_view_validation_set_top10_labels_patients_and_diagnoses.sql
+++ sql_setup/create_materialized_view_validation_set_top10_labels_patients_and_diagnoses.sql
@@ -1,7 +1,7 @@
 CREATE MATERIALIZED VIEW validation_set_top10_labels_patients_and_diagnoses AS (
     WITH patients_validation_set_with_top10_labels_who_have_notes AS (
       SELECT DISTINCT subject_id
-      FROM training_set_top10_labels
+      FROM validation_set_top10_labels
       ORDER BY subject_id ASC
     )
     SELECT patients_validation_set_with_top10_labels_who_have_notes.subject_id, diagnoses_icd.icd9_code
